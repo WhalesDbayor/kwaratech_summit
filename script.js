01c1d50f103b18f797aa62de91ba9285f3f0af76
Whales D'bayor <wabalonline01@gmail.com>
2025-07-20 17:25:56 +0100

    Add reels and hamburger menu

--- script.js
+++ script.js
@@ -24,29 +24,74 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // function updateCarouselPrev() {
-    //     carousel.style.transform = `translateX(-100%)`;
-    // }
-
     function updateCarousel() {
         carousel.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
     }
     
     // Sticky navbar
-    const navbar = document.querySelector('.navbar');
+    const header = document.querySelector('.header');
     const heroSection = document.querySelector('.hero');
     
     window.addEventListener('scroll', function() {
         if (window.scrollY > 50) {
-            navbar.style.padding = '10px 50px';
-            navbar.style.boxShadow = '0 2px 10px rgba(0, 0, 0, 0.1)';
+            header.style.padding = '10px 50px';
+            header.style.boxShadow = '0 2px 10px rgba(0, 0, 0, 0.1)';
         } else {
-            navbar.style.padding = '15px 50px';
-            navbar.style.boxShadow = 'none';
+            header.style.padding = '15px 50px';
+            header.style.boxShadow = 'none';
         }
     });
 
-    // window.addEventListener('click', function () {
-        
-    // });
+    // Hero Section Auto Slide
+    const slides = document.querySelectorAll('.slide');
+    let current = 0;
+    const intervalTime = 4000; // 4 seconds
+
+    function nextSlide() {
+    slides[current].classList.remove('active');
+    current = (current + 1) % slides.length;
+    slides[current].classList.add('active');
+    }
+
+    setInterval(nextSlide, intervalTime);
+
+
 });
+
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+// scroll sections active link
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.screenY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    // sticky navbar
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    //remove toggle icon and navbar when click navbar link (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
